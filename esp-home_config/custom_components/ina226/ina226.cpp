14356e21c27f1bf304baf4bec2029099414d5e69
kev300 <kevin@gadeco.de>
2023-08-22 15:34:50 +0200

    try to smooth inverter amps a bit more.

--- esp-home_config/custom_components/ina226/ina226.cpp
+++ esp-home_config/custom_components/ina226/ina226.cpp
@@ -46,13 +46,13 @@ void INA226Component::setup() {
   uint16_t config = 0x0000;
 
   // Averaging Mode AVG Bit Settings[11:9] (000 -> 1 sample, 001 -> 4 sample, 111 -> 1024 samples)
-  config |= 0b0000101000000000; // <- 101 - 256
+  config |= 0b0000110000000000; // <- 110 - 512
 
   // Bus Voltage Conversion Time VBUSCT Bit Settings [8:6] (100 -> 1.1ms, 111 -> 8.244 ms)
-  config |= 0b0000000010000000;  // <- 010 - 332µs
+  config |= 0b0000000000000000;  // <- 000 - 140µs
 
   // Shunt Voltage Conversion Time VSHCT Bit Settings [5:3] (100 -> 1.1ms, 111 -> 8.244 ms)
-  config |= 0b0000000000101000; // <- 101 - 2.2ms
+  config |= 0b0000000000100000; // <- 100 - 1.1ms
 
   // Mode Settings [2:0] Combinations (111 -> Shunt and Bus, Continuous)
   config |= 0b0000000000000111;
