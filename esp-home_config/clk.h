bb827d844538710e629370d6650161eb333c18ee
kev300 <kevin@gadeco.de>
2025-01-13 10:18:00 +0100

    replace cold mode with regular maxCharge limit + fix clk.h for idf 5

---
+++ esp-home_config/clk.h
@@ -0,0 +1,5 @@
+// For uint32_t esp_clk_cpu_freq() returning CPU frequence in Hz
+// Usage: ESP_LOGI("CPU", "CPU freq: %d", esp_clk_cpu_freq());
+
+//#include "soc/esp32/clk.h"
+#include "esp_private/esp_clk.h"
